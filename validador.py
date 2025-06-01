c1c05eb59d160d14d19ba49c9f7a4cc970e11e0d
Carmela <c.ruiz.18@hotmail.com>
2025-05-31 19:41:18 -0500

    versión 1: Validacion de longitud minima de la contraseña

---
+++ validador.py
@@ -0,0 +1,6 @@
+contraseña = input("Ingresa una contraseña: ")
+
+if len(contraseña) >= 8:
+    print("La contraseña es suficientemente larga.")
+else:
+    print("La contraseña es muy corta. Debe tener al menos 8 caracteres.")
